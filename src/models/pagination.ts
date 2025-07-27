756d1b5f603b505273be2f244ed77a64566c79c5
sina-saberi <94939034+sina-saberi@users.noreply.github.com>
2025-07-27 23:33:26 +0330

    fix(pagination): fix pagination in cusomter list

--- src/models/pagination.ts
+++ src/models/pagination.ts
@@ -14,19 +14,18 @@ export class Pagination<TData> {
         this.page = parseInt(page as string) || 1;
         this.pageSize = parseInt(pageSize as string) || 10;
         this.start = (this.page - 1) * this.pageSize;
-        this.end = this.start + this.page;
+        this.end = this.start + this.pageSize;
     }
 
 
     getResponse(data: TData[], res: Response) {
-        const paged = data.slice(this.start, this.end);
 
         res.json({
             total: data.length,
             page: this.page,
             pageSize: this.pageSize,
             totalPages: Math.ceil(data.length / this.pageSize),
-            data: paged
+            data: data
         });
     }
 }
